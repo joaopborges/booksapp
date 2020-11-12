9fd53d006341217c6bb65517b4fd77de38110a6d
joaopborges <joaopedro.goncalvesboges@gmail.com>
2020-11-12 11:32:09 +0000

    register

--- bibliotec/urls.py
+++ bibliotec/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from books import views as book_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('teca.urls')),
+    path('register/', book_views.register, name='register'),
+    path('', include('teca.urls')),
+
 ]
--- teca/views.py
+++ teca/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from .models import Post
+
+posts = [
+	{
+		'author':'test',
+		'title':'titletest',
+		'content':'contenttest'
+
+	},
+	{
+		'author':'errortest',
+		'title':'errortesttitle',
+		'content':'contenttesteerror'
+
+	},
+	{
+		'author':'test2',
+		'title':'titletest2',
+		'content':'contenttest2'
+
+	}
+]
+
+
+
 
 def home(request):
-	return render(request, 'teca/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'teca/home.html', context)
 
 def help(request):
 	return render(request, 'teca/help.html', {'title': 'Help'})
